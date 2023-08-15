ea206e2edf0707a3dd07b5999e3209c218648fd1
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-08-15 17:33:46 +0600

    deposit escalated to total balance

--- deposit.js
+++ deposit.js
@@ -1,13 +1,18 @@
 let depositSum = 0;
+let balanceSum = 1240;
 document.getElementById("btn-deposit").addEventListener("click", function () {
   const depositField = document.getElementById("deposit-field");
   const deposit = depositField.value;
   const depositTotal = document.getElementById("deposit-total");
+  const balanceEl = document.getElementById("balance-total");
+
   if (deposit.length === 0) {
     alert("You haven't entered any amount to deposit.");
   } else {
     depositSum += parseFloat(deposit);
     depositTotal.innerText = "$" + depositSum;
+    balanceSum += parseFloat(deposit);
+    balanceEl.innerText = balanceSum;
     depositField.value = "";
   }
 });
